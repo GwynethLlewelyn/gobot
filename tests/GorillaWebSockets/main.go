446fab87dcbee278dfa6e0f8ef3bc519bad5572a
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2020-01-22 10:45:26 +0000

    Testing ‘go fmt’ command on these files

--- tests/GorillaWebSockets/main.go
+++ tests/GorillaWebSockets/main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "fmt"
- //   "io"
-    "log"
-    "net/http"
- //   "github.com/gorilla/websocket"
+	"fmt"
+	//   "io"
+	"log"
+	"net/http"
+	//   "github.com/gorilla/websocket"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Home Page")
+	fmt.Fprintf(w, "Home Page")
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-    // upgrade this connection to a WebSocket
-    // connection
-    ws, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-    }
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
 
-    // helpful log statement to show connections
-    log.Println("Client Connected")
-    err = ws.WriteMessage(1, []byte("Hi Client!"))
-    if err != nil {
-        log.Println(err)
-    }
+	// helpful log statement to show connections
+	log.Println("Client Connected")
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
 
-    Reader(ws)
+	Reader(ws)
 }
 
 func setupRoutes() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func main() {
-    fmt.Println("Hello World")
-    setupRoutes()
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
